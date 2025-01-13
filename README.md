b7b0716e68ba1f1b892b98dea2946cac2aa90c5c
JD <jd@rudiejd.io>
2025-01-13 16:28:21 -0500

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # gtfs-duckdb
-Scripts for opening a GTFS feed in duckdb
+Script that gives you the ability to query [GTFS](https://github.com/google/transit) like a database.
+
+# Prerequisites
+- [ASDF version manager](https://asdf-vm.com/)
+- Bash 
+
+# Installation
+1. Clone the repository
+2. In the repository directory, run `asdf install`
+3. Make sure the script is executable (e.g. `chmod +x ./generate_duckdb_sql.sh` in Unix-like shells)
+4. Run the script against an unzipped GTFS directory e.g. `./genereate_duckdb_sql.sh ~/jd/path/to/gtfs/folder output_db`
+5. This launches a DuckDB shell, where you can query GTFS like a SQL database
+
--- generate_duckdb_sql.sh
+++ generate_duckdb_sql.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+if [[ $# -ne 2 ]]; then
+   echo "Usage: $0 (gtfs_dir) (output_db_name)";
+   echo "'gtfs_dir' is the directory of the unzipped gtfs bundle you want to turn into a SQL database";
+   echo "'output_db_name' is the name of your new GTFS database";
+   echo "You can then open the database using 'duckdb .(output_db_name)";
+   exit 1;
+fi
+
+duckdb_createdb_file_name="$2.duckdb.sql"
 for file in $1/*; do
     if [[ $file == *.txt ]]; then
        base=$(basename ${file})
-       echo "CREATE TABLE ${base%%.*} AS" >> $2
-       echo " SELECT * FROM read_csv('$file');" >> $2
+       echo "CREATE TABLE ${base%%.*} AS" >> $duckdb_createdb_file_name
+       echo " SELECT * FROM read_csv('$file', sample_size = -1);" >> $duckdb_createdb_file_name
     fi
 done
+
+duckdb_db_filename="$2"
+echo "EXPORT DATABASE '$2';" >> $duckdb_createdb_file_name
+
+# create the database by running the create DB script
+duckdb $duckdb_db_filename < $duckdb_createdb_file_name;
+
+# open the DB interactively
+duckdb $duckdb_db_filename
